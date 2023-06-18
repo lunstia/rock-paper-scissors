f655f4aa1ffd965450b26854d3f24bc4b449e204
lunstia <136163666+lunstia@users.noreply.github.com>
2023-06-18 07:37:51 -0400

    Add getPlayerChoice and determineWinner

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 const GAME_CHOICES = ["rock", "paper", "scissors"];
 
+
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3);
 
     return GAME_CHOICES[randomChoice];
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    let playerChoice = prompt("Type in your choice! eg. rock, paper, or scissors");
+    if (!isNaN(playerChoice) || GAME_CHOICES.indexOf(playerChoice) === -1) {
+        alert("Invalid response, try again!")
+        return getPlayerChoice()
+    }
+
+    playerChoice.toLowerCase()
+
+    return playerChoice;
+}
+
+function determineWinner(playerChoice, computerChoice) {
+    switch(playerChoice, computerChoice) {
+        case playerChoice === computerChoice:
+            return "It's a tie!"
+        case playerChoice === "rock" && computerChoice === "scissors":
+            return "You win!"
+        case playerChoice > computerChoice:
+            return "You win!"
+
+        default:
+            return "You lose!"
+    }
+}
